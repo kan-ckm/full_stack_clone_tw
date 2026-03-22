cfa3dc3cd1f9313c827db3f3fa656d35bf994a8d
tomato <at2356116@gmail.com>
2026-03-22 13:50:26 +0700

    comment chi tiet hon cho cau hinh graphql

--- backend/src/core.module.ts
+++ backend/src/core.module.ts
@@ -5,19 +5,22 @@ import { GraphQLModule } from '@nestjs/graphql'
 
 import { PrismaModule } from './core/prisma/prisma.module'
 import { getGraphQLConfig } from './core/config/graphql.config'
+import { RedisModule } from './core/redis/redis.module';
 //module trung tâm dùng để tập hợp và cấu hình các thành phần cốt lõi của ứng dụng.
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
         }),
+        // dùng để tạo graphsql nhưng config linh động hơn và giúp graphql chắc chắn đọc được file env 
         GraphQLModule.forRootAsync({
             driver: ApolloDriver,
             imports:[ConfigModule],
-            useFactory:getGraphQLConfig,
+            useFactory:getGraphQLConfig,// giúp bật giao diện quản lý truy vấn web của graphql
             inject:[ConfigService]
         }),
-        PrismaModule
+        PrismaModule,
+        RedisModule
     ]
 })
 export class CoreModule {}
