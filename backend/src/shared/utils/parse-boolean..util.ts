b5f5ed45199189ca1bbc9a3171c89553bfaf7c2f
tomato <at2356116@gmail.com>
2026-03-23 20:53:51 +0700

    tao hàm dùng để chỉnh từ dạng chuỗi sang dạng boolean

---
+++ backend/src/shared/utils/parse-boolean..util.ts
@@ -0,0 +1,24 @@
+//Hàm dùng để chuyển từ giá trị thường "string" một là "true 2 là false dạng chuỗi"  thành kiểu boolean
+
+export function parseBoolean(value: string): boolean {  // thiết lập cho tham số đầu vào là string và trả về là kiểu boolean
+
+    if (typeof value === 'boolean') {
+        return value
+    }
+
+    if (typeof value === 'string') {
+        const lowerValue = value.trim().toLocaleLowerCase()
+
+        if (lowerValue === 'true') {
+            return true
+        }
+
+        if (lowerValue === 'false') {
+            return false
+        }
+    }
+
+    throw new Error(
+        `Không thể chuyển đổi giá trị "${value}" thành kiểu boolean`
+    )
+}
