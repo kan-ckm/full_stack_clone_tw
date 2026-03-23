a736f2b322e1cac3be241f576045699f5f630d9f
tomato <at2356116@gmail.com>
2026-03-23 20:56:03 +0700

    setup ban đầu cho redis

--- backend/src/core.module.ts
+++ backend/src/core.module.ts
@@ -3,21 +3,22 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 
-import { PrismaModule } from './core/prisma/prisma.module'
 import { getGraphQLConfig } from './core/config/graphql.config'
-import { RedisModule } from './core/redis/redis.module';
+import { PrismaModule } from './core/prisma/prisma.module'
+import { RedisModule } from './core/redis/redis.module'
+
 //module trung tâm dùng để tập hợp và cấu hình các thành phần cốt lõi của ứng dụng.
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true
         }),
-        // dùng để tạo graphsql nhưng config linh động hơn và giúp graphql chắc chắn đọc được file env 
+        // dùng để tạo graphsql nhưng config linh động hơn và giúp graphql chắc chắn đọc được file env
         GraphQLModule.forRootAsync({
             driver: ApolloDriver,
-            imports:[ConfigModule],
-            useFactory:getGraphQLConfig,// giúp bật giao diện quản lý truy vấn web của graphql
-            inject:[ConfigService]
+            imports: [ConfigModule],
+            useFactory: getGraphQLConfig, // giúp bật giao diện quản lý truy vấn web của graphql
+            inject: [ConfigService]
         }),
         PrismaModule,
         RedisModule
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,30 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+
+// cách ghi * as cookieParser có nghĩa là import tất cả thư viện đó dưới tên biến là cookieParser
 
 import { CoreModule } from './core.module'
 
+// các ghi * as cookieParser có nghĩa là import tất cả thư viện đó dưới tên biến là cookieParser
 async function bootstrap() {
     const app = await NestFactory.create(CoreModule)
-    await app.listen(process.env.PORT ?? 3000)
+
+    const config = app.get(ConfigService)
+    app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true
+        })
+    )
+
+    app.enableCors({
+        origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+        credentials: true,
+        exposeHeaders: ['set-cookie']
+    })
+
+    await app.listen(config.getOrThrow<number>('APPLICATION_PORT'))
 }
 bootstrap()
